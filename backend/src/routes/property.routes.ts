ebd06cc23f2dad542f128ba0ef194ceee844ed0e
jerinbenny <jerinbenny875@gmail.com>
2026-08-21 15:51:42 +0530

    successfully completed get,update,create property module

--- backend/src/routes/property.routes.ts
+++ backend/src/routes/property.routes.ts
@@ -3,7 +3,7 @@ import authenticate from '../middleware/auth.middileware.js'
 import {validate} from '../middleware/validator.middleware.js'
 import {autihorize} from '../middleware/role.middleware.js'
 import {ROLES} from '../constants/roles.js'
-import {createPropertySchema} from  '../validator/property.validator.js'
+import {createPropertySchema,updatePropertySchema} from  '../validator/property.validator.js'
 import * as propertyController from '../controllers/property.controller.js'
 const router = Router()
 router.post("/",authenticate,autihorize(ROLES.Admin,ROLES.Manager),validate(createPropertySchema),propertyController.create)
@@ -14,5 +14,5 @@ router.get(
     autihorize(ROLES.Admin, ROLES.Manager),
     propertyController.getPropertyById
 );
-router.patch("/:id",authenticate,autihorize(ROLES.Admin,ROLES.Manager),propertyController.updateProperty)
+router.patch("/:id",authenticate,autihorize(ROLES.Admin,ROLES.Manager),validate(updatePropertySchema),propertyController.updateProperty)
 export default router; 
